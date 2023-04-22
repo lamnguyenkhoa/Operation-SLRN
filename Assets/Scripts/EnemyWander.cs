16f3a686d7fefc48a56aee6b893813765b73eaa2
NKLam <khoalamvn@gmail.com>
2023-04-23 03:23:48 +1000

    Add basic graphic and music

--- Assets/Scripts/EnemyWander.cs
+++ Assets/Scripts/EnemyWander.cs
@@ -1,46 +1,88 @@
 using UnityEngine;
 
+public enum EnemyMood
+{
+    WANDERING,
+    TIRED,
+    CHASING
+}
+
 public class EnemyWander : MonoBehaviour
 {
     public float moveSpeed = 2f;  // the speed at which the enemy moves
-    public float rotateSpeed = 100f;  // the speed at which the enemy rotates
+    private float rotateSpeed = 100f;  // the speed at which the enemy rotates
     public float minTime = 1f;  // the minimum time the enemy will walk in one direction
     public float maxTime = 4f;  // the maximum time the enemy will walk in one direction
-
-    private float timer;  // the timer to keep track of how long the enemy has been walking in one direction
+    private float walkingTimer;  // the timer to keep track of how long the enemy has been walking in one direction
     private Vector2 direction;  // the direction the enemy is currently walking in
     private Rigidbody2D rb;  // the rigidbody component of the enemy
     public GameObject pulseEffectPrefab;
+    private float pulseTimer;
+    private float timeUntilNextPulse = 0.5f;
+    private EnemyMood mood;
+    private float chaseTimer;
+    public float timeUntilStopChase = 10f; // Enemy stop chasing if after this time
+    public float distanceUntilStopChase = 10f; // Enemy stop chasing submarine outside this range
+
+    private float tiredTimer;
+    public float timeInTiredMood = 2f;
+
 
     void Start()
     {
-        timer = Random.Range(minTime, maxTime);  // set the initial timer to a random value
+        walkingTimer = Random.Range(minTime, maxTime);  // set the initial timer to a random value
         direction = Random.insideUnitCircle.normalized;  // set the initial direction to a random vector of length 1
         rb = GetComponent<Rigidbody2D>();  // get the rigidbody component
     }
 
+    void Update()
+    {
+        pulseTimer += Time.deltaTime;
+    }
+
     void FixedUpdate()
     {
-        // move the enemy in the current direction
-        Vector2 velocity = direction * moveSpeed;
-        rb.velocity = velocity;
-
-        // rotate the enemy to face the current direction
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
-        float targetAngle = Mathf.MoveTowardsAngle(transform.eulerAngles.z, angle, rotateSpeed * Time.deltaTime);
-        transform.rotation = Quaternion.Euler(0f, 0f, targetAngle);
-
-        // decrement the timer and change direction if the timer has expired
-        timer -= Time.fixedDeltaTime;
-        if (timer <= 0f)
+        if (mood == EnemyMood.WANDERING)
         {
-            direction = Random.insideUnitCircle.normalized;  // set the new direction to a random vector of length 1
-            timer = Random.Range(minTime, maxTime);  // reset the timer to a random value
+            // move the enemy in the current direction
+            Vector2 velocity = direction * moveSpeed;
+            rb.velocity = velocity;
+
+            // rotate the enemy to face the current direction
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+            float targetAngle = Mathf.MoveTowardsAngle(transform.eulerAngles.z, angle, rotateSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Euler(0f, 0f, targetAngle);
+
+            // decrement the timer and change direction if the timer has expired
+            walkingTimer -= Time.fixedDeltaTime;
+            if (walkingTimer <= 0f)
+            {
+                direction = Random.insideUnitCircle.normalized;  // set the new direction to a random vector of length 1
+                walkingTimer = Random.Range(minTime, maxTime);  // reset the timer to a random value
+            }
         }
+        else
+        {
+
+        }
+
     }
 
     public void Pulse()
     {
-        Instantiate(pulseEffectPrefab, transform.position, Quaternion.identity);
+        if (pulseTimer > timeUntilNextPulse)
+        {
+            Instantiate(pulseEffectPrefab, transform.position, Quaternion.identity);
+            pulseTimer = 0;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        SubmarineController submarine = col.gameObject.GetComponent<SubmarineController>();
+        if (submarine)
+        {
+            Debug.Log("Detected");
+        }
     }
 }
--- Assets/Scripts/SubmarineController.cs
+++ Assets/Scripts/SubmarineController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.Rendering.Universal;
 
 public class SubmarineController : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class SubmarineController : MonoBehaviour
     private float moveHorizontal;
     private float moveVertical;
     public TextMeshProUGUI coordText;
-
+    public Light2D[] buttonLights;
 
     void Start()
     {
@@ -26,9 +27,22 @@ public class SubmarineController : MonoBehaviour
         moveHorizontal = Input.GetAxis("Horizontal");
         moveVertical = Input.GetAxis("Vertical");
 
+        // Turn on button light according to movement for
+        buttonLights[0].volumeIntensityEnabled = false;
+        buttonLights[1].volumeIntensityEnabled = false;
+        buttonLights[2].volumeIntensityEnabled = false;
+        buttonLights[3].volumeIntensityEnabled = false;
+        if (moveHorizontal < 0)
+            buttonLights[0].volumeIntensityEnabled = true;
+        else if (moveHorizontal > 0)
+            buttonLights[1].volumeIntensityEnabled = true;
+        if (moveVertical < 0)
+            buttonLights[2].volumeIntensityEnabled = true;
+        else if (moveVertical > 0)
+            buttonLights[3].volumeIntensityEnabled = true;
+
         // Move the player based on the input axes
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
         rb.velocity = movement * moveSpeed;
     }
-
 }
